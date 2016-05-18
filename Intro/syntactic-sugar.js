ed3ac08482e53c1c31011a6dafb220180d265622
hwatson <hwatson@squiz.net>
2016-05-18 11:34:57 +1000

    Syntatic Sugar Example

---
+++ Intro/syntactic-sugar.js
@@ -0,0 +1,20 @@
+/**
+ * Functions are already a feature of Javascript
+ */
+
+/**
+ * ES5 Style
+ */
+function hello() {
+    console.log('hello');
+}
+
+hello();
+
+/**
+ * ES6 Style Arrow Function
+ */
+const hello2 = () => console.log('hello 2');
+
+hello2();
+
